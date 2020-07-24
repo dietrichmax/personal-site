9b369b20389fc6299bdacc805ea48a8867ba0f86
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2020-07-25 01:04:02 +0200

    removed category color

--- wrapPageElement.js
+++ wrapPageElement.js
@@ -4,8 +4,8 @@ import { Layout } from './src/components/Layout';
 
 
 // Pass all props (hence the ...props) to the layout component so it has access to things like pageContext or location
-const wrapPageElement = ({ element, props, categoryColor }) => (
-  <Layout {...props}{...categoryColor}>{element}</Layout>
+const wrapPageElement = ({ element, props }) => (
+  <Layout {...props}>{element}</Layout>
 );
 
 export default wrapPageElement;

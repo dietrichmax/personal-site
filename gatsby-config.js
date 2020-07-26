f5dee4f9ffa16233816e518d261edb7e8201ea36
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2020-07-26 23:39:56 +0200

    removed image unwrap

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     "gatsby-plugin-preact",
     "netlify-plugin-gatsby-cache",
-    "gatsby-remark-unwrap-images",
     {
       resolve: `gatsby-plugin-sass`,
       options: {

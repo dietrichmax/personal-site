a902d720cd861965bbcbdf1382687a644a0973a5
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2020-07-27 17:58:28 +0200

    performance

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,15 @@
 import 'lazysizes';
 import CustomLayout from './wrapPageElement';
 import firebase from 'gatsby-plugin-firebase';
-import 'firebase/database';
-import "firebase/performance";
 
 require('prismjs/themes/prism-tomorrow.css');
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Performance Monitoring and get a reference to the service
+const perf = firebase.performance();
+const perf = firebase.database();
 
 
 

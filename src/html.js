86bf779ce174f76b175aa696f805e3b91aa3b61a
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2020-08-03 20:49:40 +0200

    verfii

--- src/html.js
+++ src/html.js
@@ -11,8 +11,8 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <meta name="purpleads-verification" content="b18117cb1237a6a6b333dda5"></meta> 
         {props.headComponents}
+        <meta name="purpleads-verification" content="b18117cb1237a6a6b333dda5"></meta> 
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}

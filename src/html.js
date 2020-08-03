216618fe3e1462804ab866050fdcf657394bcfa7
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-08-03 16:13:02 +0200

    Update html.js
    purple ads verification

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <meta name="purpleads-verification" content="b18117cb1237a6a6b333dda5">
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}

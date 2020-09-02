d4c0af05ee580721d2935f5ce0aff0ed5c8df673
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-09-02 18:34:15 +0200

    Update container.js

--- components/layout/container.js
+++ components/layout/container.js
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 import media from 'styled-media-query';
 
 const ContainerWrapper = styled.div`
-  max-width: 1140px;
-  margin: auto;
+
 `
 
 export default function Container({ children }) {

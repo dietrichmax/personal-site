3aca25b54467ba914ba0685c40ba4874534c4873
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-09-03 16:49:51 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,27 @@ import { useEffect } from 'react'
 import Router from 'next/router'
 import * as gtag from '../lib/gtag'
 
+function isDev() {
+  return process.env.NODE_ENV !== "production";
+}
+
 const App = ({ Component, pageProps }) => {
   useEffect(() => {
     const handleRouteChange = (url) => {
       gtag.pageview(url)
     }
-    Router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      Router.events.off('routeChangeComplete', handleRouteChange)
+    
+    const isDevelopment = isDev();
+    
+    if (isDevelopment) {
+      console.log("Tracking is disabled in development mode")
+    } else {
+      Router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        Router.events.off('routeChangeComplete', handleRouteChange)
+      }
     }
+    
   }, [])
 
   return <Component {...pageProps} />

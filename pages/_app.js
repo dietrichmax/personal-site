94f7ba647a5e2f1eebda826a4a47e331fc103ace
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-09-03 16:37:50 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
 import '@/styles/index.css'
-import Router from "next/router";
-import withGA from "next-ga";
+import { useEffect } from 'react'
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
 
-function MyApp({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
-export default withGA(process.env.GA_ID, Router)(MyApp);
+export default App

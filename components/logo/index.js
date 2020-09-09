4d3f977abe69bc86540723b552301d4be56c9ee2
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-09-09 13:24:13 +0200

    Update index.js

--- components/logo/index.js
+++ components/logo/index.js
@@ -17,7 +17,7 @@ export const LogoWrapper = styled(Img)`
 export default function Logo() {
   return (
     <Link href={config.homePath}>
-      <LogoWrapper src={logo} alt={config.siteTitle} title={config.siteTitle}/>
+      <LogoWrapper src={logo} webp sizes={[40]} alt={config.siteTitle} title={config.siteTitle}/>
     </Link>
   )
 }

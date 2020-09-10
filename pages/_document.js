e90454bebdc866363987274cdceea39ba4a7ea45
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-09-10 14:58:43 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', ''${process.env.GA_ID}'', { 'anonymize_ip': true });
+              gtag('config', '${process.env.GA_ID}', { 'anonymize_ip': true });
               `,
            }}
           />

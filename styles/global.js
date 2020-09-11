81fc601eace744fdbc697b3166903d3e9f94e383
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-09-11 15:26:04 +0200

    Update global.js

--- styles/global.js
+++ styles/global.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,

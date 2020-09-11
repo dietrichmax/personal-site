301c679f5d7329efbd4386fa7c66259e0af8d410
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-09-11 15:49:10 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,19 +5,14 @@ import { useAnalytics } from "../lib/useGA";
 
 function MyApp({ Component, pageProps }) {
 
-  // Check if in production
-  const isProduction = process.env.NODE_ENV === 'production'
-
   const { init, trackPageViewed } = useAnalytics();
 
   useEffect(() => {
-    if (isProduction) {
       init("UA-117248551-1");
       trackPageViewed();
       Router.events.on("routeChangeComplete", () => {
         trackPageViewed();
       });
-    }
   }, []);
 
   return (

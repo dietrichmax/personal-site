13a0f049eb76aa3832a3f1ff74e78321072332bb
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-09-15 14:17:01 +0200

    Update stellenanzeige-erstellen.js

--- pages/jobs/stellenanzeige-erstellen.js
+++ pages/jobs/stellenanzeige-erstellen.js
@@ -109,13 +109,6 @@ export default function Recruiting({ }) {
                 label="job-description-input"
                 placeholder="Job Beschreibung"
               />
-<Input
-                type="text"
-                name="Job Description"
-                id="job-description"
-                label="job-description-input"
-                placeholder="Job Beschreibung"
-              />
                 <JobAdvertiseButton
                   type="button"
                   aria-label="Abonnieren"

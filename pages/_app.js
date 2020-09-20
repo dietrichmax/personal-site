82b0762688d56d28f02bd84c30472cffc6bf3d6c
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2020-09-21 00:02:49 +0200

    font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Router from "next/router";
 import React, { useEffect, useState } from "react";
-//import '@/styles/global.css'
-import GlobalStyle from '@/styles/global.js'
+import '@/styles/global.css'
+//import GlobalStyle from '@/styles/global.js'
 import { useAnalytics } from "../lib/useGA";
 import CookieBanner from '@/components/cookies/cookie-banner'
 
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <GlobalStyle/>
+      {/*<GlobalStyle/>*/}
       <Component {...pageProps} />
       <CookieBanner/>
     </>

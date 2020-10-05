ce75ab0ef120aa345c87b7665d26d724a90f2e55
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2020-10-05 22:38:43 +0200

    live count

--- pages/statistiken.js
+++ pages/statistiken.js
@@ -177,7 +177,7 @@ export default function Recruiting({ lastViews, liveViews, actions }) {
           <Container>
             <GeneralStats>
               {live > 1 ?
-              <a>Du bist derzeit mit {live - 1} weiteren Person auf GIS-Netzwerk.com. </a> :
+              <a>Du bist derzeit mit {live - 1} weiteren Personen auf GIS-Netzwerk.com. </a> :
               <a>Du bist momentan die einzige Person auf GIS-Netzwerk.com. </a>
               }
               <br/>

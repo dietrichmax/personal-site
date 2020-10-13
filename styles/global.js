8c0f06413eb31f7bd1df32fb2c65bad5c15dc5f9
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-10-13 23:39:00 +0200

    Update global.js

--- styles/global.js
+++ styles/global.js
@@ -20,6 +20,7 @@ const GlobalStyle = createGlobalStyle`
     vertical-align: baseline;
     text-decoration: none;
     overflow-wrap: break-word;
+    font-display: fallback;
   }
   a {
     transition: 0.2s;
@@ -55,7 +56,6 @@ const GlobalStyle = createGlobalStyle`
     font-weight: 400;
     font-family: system-ui,-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
     font-size: 1.5rem;
-    font-display: auto;
   }
   img {
     display: block;

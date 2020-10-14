f0c80c4349f6a26eb129dd7c278ed04f2b4e12fe
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-10-14 11:33:11 +0200

    Update cover-image.js

--- components/post/post-image/cover-image.js
+++ components/post/post-image/cover-image.js
@@ -23,7 +23,7 @@ const PreviewCoverImage = styled(Img)`
     props.hero ? "none" : "0.75rem" };
   border-top-right-radius: ${props =>
     props.hero ? "none" : "0.75rem" };
-  max-width: ${props =>
+  width: ${props =>
     props.hero ? "100%" : "400px" };;
   height: ${props =>
     props.hero ? "400px" : '200px'};

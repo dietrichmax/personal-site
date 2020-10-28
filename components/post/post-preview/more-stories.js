07d74db51a210fba4c4c5434f339c2e5fd86f7b7
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-10-28 10:29:39 +0100

    Update more-stories.js

--- components/post/post-preview/more-stories.js
+++ components/post/post-preview/more-stories.js
@@ -7,7 +7,7 @@ const ListWrapper = styled.div`
   margin: var(--space);
   display: grid;
   grid-gap: 20px;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(330px, 1fr));
 `;
 
 

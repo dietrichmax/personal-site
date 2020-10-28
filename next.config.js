8aba6d25a24118fd30d725419394c17f9c356781
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2020-10-28 10:47:34 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -275,12 +275,12 @@ const redirects = {async redirects() {
   ]
 }};
 
-
 module.exports = withPlugins([
   [mdx],
   redirects,
   {images: {
     deviceSizes: [320, 420, 768, 1024, 1200],
     domains: ['api.mxd.codes'],
+    path: 'https://api.mxd.codes',
   }}
 ]);

4c7397eb7ce8c4bdbe0b636d0b1cb206bfce5d34
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2021-01-30 11:20:56 +0100

    seo

--- components/seo/seo.js
+++ components/seo/seo.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import config, { dateFormat } from '../../lib/data/SiteConfig';
+import Head from "next/head"
+import config, { dateFormat } from "../../lib/data/SiteConfig";
 
 const SEO = ({ 
   title,
@@ -15,16 +15,16 @@ const SEO = ({
   title = title ? title : config.siteTitle
   description  = description ? description : config.siteDescription
   slug = slug ? slug : config.siteUrl
-  image = image ? `${image.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''}${image}` : `${config.siteUrl}${config.siteLogo}`
+  image = image ? `${image.startsWith("/") ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ""}${image}` : `${config.siteUrl}${config.siteLogo}`
   date = date ? date : new Date()
-  ogType = ogType ? ogType : 'website'
+  ogType = ogType ? ogType : "website"
   
 
 
   const schemaOrgJSONLD = [
     {
-      '@context': 'http://schema.org',
-      '@type': 'WebSite',
+      "@context": "http://schema.org",
+      "@type": "WebSite",
       url: config.siteUrl,
       name: config.siteTitle,
       alternateName: config.siteTitleAlt
@@ -33,40 +33,14 @@ const SEO = ({
   if (postSEO) {
     schemaOrgJSONLD.push(
       {
-        '@context': 'http://schema.org',
-        '@type': 'BreadcrumbList',
-        itemListElement: [
-          {
-            '@type': 'ListItem',
-            position: 1,
-            item: {
-              '@id': slug,
-              name: title,
-              image: image,
-            }
-          }
-        ]
-      },
-      {
-        '@context': 'http://schema.org',
-        '@type': 'BlogPosting',
-        url: slug,
-        name: title,
-        alternateName: config.siteTitleAlt,
-        headline: title,
-        datePublished: date,
-        dateModifed: date,
-        mainEntityOfPage: slug,
-        image: {
-          '@type': 'ImageObject',
-          url: image,
-        },
-        description,
-        author: config.userName,
-        datePublished: date,
-        dateModified: date,
-        publisher: config.siteTitle,
-
+        "@context": "http://schema.org",
+        "@type": "NewsArticle",
+        "headline": title,
+        "image": [
+          image,
+         ],
+        "datePublished": date,
+        "dateModified": date
       }
     );
   } 
@@ -75,37 +49,37 @@ const SEO = ({
     <Head>
       {/* General tags */}
       <title>{title} - {config.siteTitleShort}</title>
-      <link rel='canonical' href={`${config.siteUrl}/${slug}`} />
-      <meta name='description' content={description} />
-      <meta name='image' content={image} />
-      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      {/*<html lang='en' />*/}
+      <link rel="canonical" href={`${config.siteUrl}/${slug}`} />
+      <meta name="description" content={description} />
+      <meta name="image" content={image} />
+      <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+      {/*<html lang="en" />*/}
       
       <link rel="manifest" href="/manifest.json" />
-      <link href='/logos/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-      <link href='/logos/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-      <link rel='apple-touch-icon' href='/logos/android-chrome-192x192.png'/>
+      <link href="/logos/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+      <link href="/logos/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+      <link rel="apple-touch-icon" href="/logos/android-chrome-192x192.png"/>
       <meta name="theme-color" content="#0a1924"/>
       {/* Schema.org tags */}
-      <script type='application/ld+json'>
+      <script type="application/ld+json">
         {JSON.stringify(schemaOrgJSONLD)}
       </script>
 
       {/* OpenGraph tags */}
-      <meta property='og:url' content={slug} /> 
-      <meta property='og:type' content={ogType} />
-      <meta name='og:title' property='og:title' content={title} />
-      <meta name='og:description' property='og:description' content={description} />
-      <meta property='og:site_name' content={title} />
-      <meta property='og:image' content={`${image}`} /> 
+      <meta property="og:url" content={slug} /> 
+      <meta property="og:type" content={ogType} />
+      <meta name="og:title" property="og:title" content={title} />
+      <meta name="og:description" property="og:description" content={description} />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:image" content={`${image}`} /> 
 
       {/* Twitter Card tags */}
-      <meta name='twitter:title' content={title} />
-      <meta name='twitter:description' content={description} />
-      <meta name='twitter:image' content={image} />
-      <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:site' content={config.socials.twitter} />
-      <meta name='twitter:creator' content={config.socials.twitter} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={config.socials.twitter} />
+      <meta name="twitter:creator" content={config.socials.twitter} />
 
 
       </Head>

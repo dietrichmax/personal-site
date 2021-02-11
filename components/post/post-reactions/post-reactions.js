06079471447ba01deaa897983aa3b1e640a0ebf6
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-11 23:11:59 +0100

    Update post-reactions.js

--- components/post/post-reactions/post-reactions.js
+++ components/post/post-reactions/post-reactions.js
@@ -2,57 +2,17 @@ import React, { useState, useEffect } from "react"
 import styled from 'styled-components';
 import media from 'styled-media-query';
 
-
-const Container = styled.div`
-  width: 20rem;
-  margin: var(--space-lg) auto;
-  padding: var(--space-sm) var(--space);
-  display: flex;
-  justify-content: space-around;
-  
-`
-
-const Title = styled.div`
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`
-
-
-const Reaction = styled.div`
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  display: flex;
-  border: 1px solid var(--secondary-color);
-  padding: var(--space-sm);
-`
-
-const Button = styled.button`
-  background-color: transparent;
-  border: none;
-  color: var(--text-color);
-  outline: none;
-
-`
-
 const Icon = styled.i`
   font-size: 30px;
   cursor: pointer;
   color: ${props => (props.incremented ? "var(--thirdy-color)" : "var(--text-color)")};
 `
 
-const Count = styled.span`
-  font-size: 1.5rem;
-  margin-top: var(--space-sm);
-
-`
 
 const PreviewLikeCount = styled.span`   
   margin-left: var(--space-sm);
 `
-const PreviewIcon = styled.i`
-`
+
 
 export default function PostReactions({ postID, preview }) {
     const [reactionId, setReactionID] = useState()
@@ -120,9 +80,3 @@ export default function PostReactions({ postID, preview }) {
     </PreviewLikeCount> 
   )
 }
-<Container>
-      <Reaction>
-        <Button ><Icon } className="las la-heart" /></Button>
-        <Count>{heart == undefined ? 0 : heart}</Count>
-      </Reaction>
-    </Container>

fdb9684b031c514fc504e41c67e53c99ccb6aff5
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-12 17:14:22 +0100

    Update content-title.js

--- components/title/content-title.js
+++ components/title/content-title.js
@@ -4,7 +4,7 @@ const Title = styled.h1`
   margin: calc(var(--space-sm)*0.5) auto var(--space) auto;
   font-size: 3rem;
   line-height: 4.5rem;
-
+  font-family: var(--secondary-font);
 `
 export default function PostTitle({ children }) {
   return (

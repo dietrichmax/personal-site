94a2f02c11ce249886db8e9525e18588b2173162
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-15 13:23:48 +0100

    Update layout.js

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -2,8 +2,8 @@ import React,  { useEffect } from "react"
 import styled from 'styled-components';
 import media from 'styled-media-query';
 const prism = require("prismjs")
+import "@/styles/prism.css"
 require('prismjs/components/prism-python');
-require('prism-themes/themes/prism-vs.css')
 
 const Container = styled.div`
 `

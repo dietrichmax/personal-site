e6234bf2ffe77e0f6b97a1a716552106041389ed
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-17 11:06:57 +0100

    Update global.js

--- styles/global.js
+++ styles/global.js
@@ -77,7 +77,7 @@ const GlobalStyle = createGlobalStyle`
     --border-light: var(--gray-light);
     --border-dark: var(--gray-dark);
     --primary-font: open-sans, sans-serif;
-    --secondary-font: 'EB Garamond', serif;
+    --secondary-font: source-serif-pro,serif;
     --thirdy-font: 'Cousine', sans-serif;
     --link-color: var(--thirdy-color);
     --link-color-hover: var(--gray);

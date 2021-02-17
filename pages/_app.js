39d2b6d7f148854084a77edaac7f1eb82abcd3a7
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-17 10:31:04 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -56,6 +56,8 @@ class MyApp extends App {
             rel="stylesheet" 
             href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/css/line-awesome.min.css" 
           />,
+          <link rel="preconnect" href="https://fonts.gstatic.com"> 
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Brygada+1918&display=swap" rel="stylesheet"> 
           <link 
             rel="preconnect" 
             href="https://use.typekit.net"

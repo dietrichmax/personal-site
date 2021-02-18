41bec78e64b88d74664e0b3a240f278c2efb922b
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-18 13:19:24 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -66,8 +66,9 @@ class MyApp extends App {
             href="https://maxst.icons8.com" 
           />,
           <link 
-            rel="preload" 
-            href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/css/line-awesome.min.css"
+            rel="preconnect" 
+            href="https://maxst.icons8.com"
+            crossorigin 
           />,
           <link 
             rel="stylesheet" 
@@ -79,8 +80,9 @@ class MyApp extends App {
             href="https://fonts.gstatic.com" 
           />,
           <link 
-            rel="preload" 
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Source+Serif+Pro:ital,wght@0,400;0,600;1,400;1,600&display=swap" 
+            rel="preconnect" 
+            href="https://fonts.googleapis.com" 
+            crossorigin 
           />,
           <link 
             rel="stylesheet"
@@ -92,8 +94,9 @@ class MyApp extends App {
             href="https://use.typekit.net" 
           />,
           <link 
-            rel="preload" 
-            href="https://use.typekit.net/xhe6fwq.css" 
+            rel="preconnect" 
+            href="https://use.typekit.net" 
+            crossorigin 
           />,
           <link 
             rel="stylesheet"  

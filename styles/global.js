1cce2e9d086734369017fee3aa8ae1a9dec6f822
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-18 11:44:19 +0100

    Update global.js

--- styles/global.js
+++ styles/global.js
@@ -55,6 +55,7 @@ const GlobalStyle = createGlobalStyle`
     font-weight: 400;
     font-style: normal; 
     font-family: var(--primary-font);
+    font-display: swap;
     word-break: break-word;
     hyphens: auto;
   }

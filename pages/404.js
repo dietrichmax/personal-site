8f966a6300bb52fbd0a8d1e39b447bfdadb9d6e2
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-18 11:06:30 +0100

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,30 +1,3 @@
-
-import Header from '@/components/navigation/header/header'
-import Footer from '@/components/navigation/footer/footer'
-import Layout from '@/components/layout/layout'
-import SEO from '@/components/seo/seo'
-import Title from '@/components/title/page-title'
-import Link from 'next/link'
-
 export default function Custom404() {
-  return (
-  <>
-    <SEO   
-      title="404 - Page not found"
-      slug=""
-    />
-    <Layout>
-      <Header/>
-      <Title>404 - Page Not Found</Title>
-      
-      <Link href="/">
-        <a>
-          Go back home
-        </a>
-      </Link>
-
-      <Footer />
-    </Layout>
-  </>
-  )
+  return <h1>404 - Page Not Found</h1>
 }

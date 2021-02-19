9235065cb968d31f6ece8401cecbd2fde33c9444
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-19 14:48:29 +0100

    Update post-body.js

--- components/post/post-body/post-body.js
+++ components/post/post-body/post-body.js
@@ -1,8 +1,25 @@
 import markdownStyles from '@/styles/markdown-styles.module.css'
 import styled from 'styled-components';
 import ReactMarkdown from "react-markdown"
-import renderers from "@/lib/utils/renderers"
+//import renderers from "@/lib/utils/renderers"
+import Image from "next/image"
+import Link from "next/link"
 
+const renderers = {
+  image: image => {
+    return <Image class="test" src={image.src} alt={image.alt} layout="responsive"/>
+  },
+  link: link => {
+    return ( 
+      link.href.startsWith("/") ?
+      <Link href={link.href} passHref> 
+        <a alt={link.alt} title={link.title}>{link.children}</a>
+      </Link> :
+      <a href={link.href} alt={link.alt} title={link.title}>{link.children}</a>
+    )
+  },
+}
+        
 const PostContent = styled.section`
   margin: var(--space-sm) 0;
 `

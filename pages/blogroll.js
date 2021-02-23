8f97f793fee5a498b5d814249c192073403de4c8
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-23 17:43:46 +0100

    Update blogroll.js

--- pages/blogroll.js
+++ pages/blogroll.js
@@ -61,13 +61,11 @@ const BlogrollLink = styled.a`
 
 const BlogrollProfileLink = styled.cite`
   display: block;
-  font-size: 1rem;
   border-bottom: 1px solid var(--link-color);
   font-family: var(--secondary-font);
 `
 
 const BlogrollProfileDesc = styled.p`
-  font-size: 18px;
   font-family: var(--secondary-font);
 `
 

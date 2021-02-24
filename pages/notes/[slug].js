237774dc5f7d39177a69439aba15a59472dc258f
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-24 12:45:23 +0100

    Update [slug].js

--- pages/notes/[slug].js
+++ pages/notes/[slug].js
@@ -43,6 +43,8 @@ const NotesContent = styled.div`
 
 const NoteInfo = styled.div`
   display: flex;
+  align-items: center;
+  margin-left: var(--space-sm);
 `
 
 const SyndList = styled.ol`
@@ -94,7 +96,7 @@ export default function Note({ note }) {
                  
                 <NoteInfo>
                   <a className="u-url" href={`${config.siteUrl}/notes/${note.date}`} rel="bookmark">
-                    <NoteMeta postMetaData={note} slug={`/notes/${note.date}`} />
+                    <NoteMeta title={note.title} postMetaData={note} slug={`/notes/${note.date}`} />
                   </a>
                   <SyndList className="relsyn">
                     {note.syndLinkTwitter ? <li><a aria-label="twitter" title="See this tweet on Twitter" className="u-syndication syn-link" href={note.syndLinkTwitter} rel="syndication"><i className="lab la-twitter"/></a></li> : null }

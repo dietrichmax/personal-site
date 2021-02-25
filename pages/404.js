2f64bce472df9296ee5dc10dcdae5ea76820bd2d
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-25 10:35:17 +0100

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,18 @@
+import Layout from '@/components/layout/layout'
+import SEO from '@/components/seo/seo'
+import Title from '@/components/title/page-title'
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  
+  return (
+    <Layout>
+  
+      <SEO   
+        title="404 - Page Not Found"
+        slug=""
+      />
+      <Title>404 - Page Not Found</Title>
+      
+    </Layout>
+  )
 }

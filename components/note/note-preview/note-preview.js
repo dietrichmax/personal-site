99e1c123c0da17a01dce8495b1dd8deb1740e79b
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-02-25 11:32:54 +0100

    Update note-preview.js

--- components/note/note-preview/note-preview.js
+++ components/note/note-preview/note-preview.js
@@ -6,13 +6,11 @@ import NoteBody from "@/components/note/note-body/note-body"
 import config from "@/lib/data/SiteConfig"
 
 const NotesItem = styled.li`
-  margin: 1px;
   overflow: hidden;
   position: relative;
   min-height: 250px;
   box-shadow: 0 2px 2px rgba(0,0,0,.09);
   background-color: var(--content-bg);
-  padding: var(--space-sm);
 `
 
 const NotesDate = styled.p`

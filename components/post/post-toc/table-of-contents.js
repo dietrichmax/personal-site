ba0d5de6a9c5dcf08cd48a92f49fb55e52b3d137
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-03-02 09:07:23 +0100

    Update table-of-contents.js

--- components/post/post-toc/table-of-contents.js
+++ components/post/post-toc/table-of-contents.js
@@ -17,6 +17,31 @@ const TOCContainer = styled.div`
 
 const TOCTitle = styled.p`
 `
+/*
+export default function MarkdownPage<
+  T extends { title: string } = { title: string }
+>({ children, meta }: MarkdownProps<T>) {
+  const anchors = React.Children.toArray(children)
+    .filter(
+      (child: any) =>
+        child.props?.mdxType && ['h2', 'h3'].includes(child.props.mdxType)
+    )
+    .map((child: any) => ({
+      url: '#' + child.props.id,
+      depth:
+        (child.props?.mdxType &&
+          parseInt(child.props.mdxType.replace('h', ''), 0)) ??
+        0,
+      text: child.props.children
+    }));
+
+  return (
+    <>
+      <MDXProvider components={MDXComponents}>{children}</MDXProvider>
+      <Toc anchors={anchors} />
+    </>
+  );
+}*/
 
 export default function TableOfContents ( content ) {
   //const size = useWindowSize()

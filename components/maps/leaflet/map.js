c19c3464c7e3d3c7ea9e83e8f501e2f9e0a4d1a7
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2021-03-03 21:04:01 +0100

    asd

---
+++ components/maps/leaflet/map.js
@@ -0,0 +1,50 @@
+/*import React from "react"
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON, Circle, latLngBound } from "react-leaflet";
+//import locations from "./locations.json"
+
+
+const Map = (data) => {
+      
+
+  
+  const bounds = []
+
+  const getVel = (vel) => {
+    if (vel == 0 && vel < 1) {
+      return "white"
+    } else if (vel > 1 && vel < 15) {
+      return "#FFFFB7"
+    } else if (vel > 15 && vel < 30) {
+      return "#FFF192"
+    } else if (vel > 30 && vel < 50) {
+      return "#FFEA61"
+    } else if (vel > 50 && vel < 60) {
+      return "#FFDD3C"
+    } else if (vel > 60 && vel < 70) {
+      return "#FFD400"
+    } else if (vel > 70 ) {
+      return "yellow"
+    } 
+  };
+
+  return (
+    <MapContainer
+      scrollWheelZoom={false}
+      style={{ height: "500px", width: "1200px" }}
+      bounds={bounds}
+    >
+      <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url='https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png'
+        />
+        {data.data.map((position,i) => {
+          bounds.push([position.lat, position.lon])
+          return (
+            <Circle center={[position.lat, position.lon]} radius={5} color={getVel(position.vel)} fillOpacity={.8} weight={0}/>
+         )
+        })}
+    </MapContainer>
+  );
+};
+
+export default Map;*/

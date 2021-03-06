50aa890d68a2d851477e1740126ff77d5f117366
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2021-03-06 14:30:32 +0100

    microformat

--- components/tags/tags.js
+++ components/tags/tags.js
@@ -32,7 +32,7 @@ export default function PostTags( tags, post) {
         <TagsWrapper layout={post}>
           {postTags.map((tag, i) => (
             <Link key={i} href={`/articles/topics/${tag.slug}`} passHref>
-              <TagItem color={tag.color} title={tag.name}>{tag.name}</TagItem>
+              <TagItem className="p-category" color={tag.color} title={tag.name}>{tag.name}</TagItem>
             </Link>
           ))}
         </TagsWrapper>

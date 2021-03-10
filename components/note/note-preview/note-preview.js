de617e61d8660a305a9b6d7a4ffcc442b3140d99
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-03-10 12:27:35 +0100

    Update note-preview.js

--- components/note/note-preview/note-preview.js
+++ components/note/note-preview/note-preview.js
@@ -14,7 +14,7 @@ const NotesItem = styled.li`
   justify-content: space-between;
   overflow: hidden;
   position: relative;
-  height: 260px;
+  height: 350px;
   box-shadow: var(--box-shadow);
   background-color: var(--content-bg);
   border-radius: var(--border-radius);

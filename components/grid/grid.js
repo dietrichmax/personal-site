3ebb5b4c6e6ca1e97bef2151914f8c51cea77289
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-03-11 11:12:45 +0100

    Update grid.js

--- components/grid/grid.js
+++ components/grid/grid.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import media from 'styled-media-query';
 
-const GridContainer = styled.ol`
+const GridContainer = styled.div`
   max-width: var(--width-container);
   padding-left: var(--space);
   padding-right: var(--space);

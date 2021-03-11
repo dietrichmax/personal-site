4e6d40028df0a4d93f4c802ba8126e236612a7e2
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-03-11 13:42:32 +0100

    Update map.js

--- components/maps/leaflet/map.js
+++ components/maps/leaflet/map.js
@@ -49,7 +49,7 @@ const Map = (data) => {
         />
         {data ? data.data.map((position,i) => {
           bounds.push([position.lat, position.lon]),
-          <Circle center={[position.lat, position.lon]} radius={5} color="lime" fillOpacity={getVel(position.vel)} weight={0}/>
+          <Circle center={[position.lat, position.lon]} radius={5} color={lime} fillOpacity={getVel(position.vel)} weight={0}/>
         })}
     </MapContainer>
   );

ffc86dd7386054567b963c42ce55fb720397dbe1
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2021-03-23 22:36:07 +0100

    osm link

--- pages/index.js
+++ pages/index.js
@@ -163,7 +163,7 @@ export default function Index({ posts, count, about }) {
                     height="275"
                   />
                 </IntroImg>
-                <HeroFont>Hi, I’m </HeroFont><HeroLinks href={config.siteUrl} title={config.siteTitle}>Max Dietrich</HeroLinks>, GeoData Manager and Web-Developer from Rosenheim, Germany. <br/>
+                <HeroFont>Hi, I’m </HeroFont><HeroLinks href={config.siteUrl} title={config.siteTitle}>Max Dietrich</HeroLinks>, GeoData Manager and Web-Developer from <a href="https://www.openstreetmap.org/search?query=rosenheim#map=13/47.8481/12.1035" title="Rosenheim, Germany">Rosenheim, Germany.</a> <br/>
                   I am also a proud member of the <HeroLinks href="https://indieweb.org/" title="IndieWeb">IndieWeb</HeroLinks> community.
                   <Link href="/about" passHref><AboutMeLink title="About me"> Read more.</AboutMeLink></Link>
                 </HeroDescription>

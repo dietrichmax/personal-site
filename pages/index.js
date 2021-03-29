388222be6bc422df9ac3172a51acaebbb550d744
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2021-03-29 20:34:59 +0200

    ads

--- pages/index.js
+++ pages/index.js
@@ -138,7 +138,7 @@ const IntroImg = styled.div`
     margin-bottom: var(--space-sm);
   `}
 `
-export default function Index({ posts, count, about }) {
+export default function Index({  count, about }) {
   const router = useRouter()
 
   return (
@@ -179,26 +179,7 @@ export default function Index({ posts, count, about }) {
              </HeroWrapper>
             <IndexPageContainer>
 
-              <Grid>
-                {posts.map((post,i) => (
-                  post.type === "article" ? (
-                    <PostPreview
-                      key={i}
-                      postData={post.post}
-                    />
-                  ) : post.type === "note" ? (
-                    <NotePreview 
-                      key={i}
-                      note={post.note} 
-                    />
-                  ) : post.type === "link" ? (
-                    <LinkPreview
-                      key={i}
-                      link={post.link} 
-                    />
-                  ) : null
-                ))}
-              </Grid>
+
 
 
               {/*{<SubTitle>Recent Notes</SubTitle>
@@ -224,15 +205,14 @@ export default function Index({ posts, count, about }) {
 
 export async function getStaticProps() {
   const about = await getAbout()
-  const resPosts = await fetch(`${server}/api/posts`)
-  const posts = await resPosts.json()
+  /*const resPosts = await fetch(`${server}/api/posts`)
+  const posts = await resPosts.json()*/
   const resStats = await fetch(`${server}/api/stats`)
   const stats = await resStats.json()
 
   return {
     revalidate:  86400,
     props: { 
-      posts,
       count: stats.posts.count,
       about: about.about,
     },

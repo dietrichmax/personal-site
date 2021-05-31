ba9d0ed98009754d7f54de58893baa9542dd04c3
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2021-05-31 22:25:00 +0200

    grid 3 columns

--- components/grid/grid.js
+++ components/grid/grid.js
@@ -5,7 +5,7 @@ const GridContainer = styled.ol`
   max-width: var(--width-container);
   padding-left: var(--space);
   padding-right: var(--space);
-  grid-template-columns: repeat(2,minmax(0,1fr));
+  grid-template-columns: repeat(3,minmax(0,1fr));
   gap: var(--space);
   display: grid;
   margin-bottom: var(--space-lg);
--- pages/index.js
+++ pages/index.js
@@ -81,7 +81,7 @@ const Grid = styled.ol`
   margin-bottom: var(--space);
   list-style: none;
   display: grid;
-  grid-template-columns: repeat(2,minmax(0,1fr));
+  grid-template-columns: repeat(3,minmax(0,1fr));
   gap: var(--space);
   ${media.lessThan('medium')`
     padding-left: var(--space-sm);

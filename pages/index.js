723190d94e121f133265c134002d59f6916d7af4
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-07-13 15:29:43 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,10 @@ const HeroWrapper = styled.div`
   width: 100%;
   margin: auto;
   background-color: var(--secondary-color);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: ${props => props.location ? {`url("https://api.mapbox.com/styles/v1/mapbox/dark-v10/static/${props.location.lon},${props.location.lat}/1280x700?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN})`} : ""} ;
+  background-blend-mode: overlay;
   `
 const Hero = styled.div`   
   display: flex;

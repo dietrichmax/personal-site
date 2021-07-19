a8c695e442347a1e59a3f220c9462883411e21a0
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-07-19 14:53:27 +0200

    Update card.js

--- styles/templates/card.js
+++ styles/templates/card.js
@@ -2,14 +2,8 @@ import styled from 'styled-components';
 
 const Card = styled.li`    
   position: relative;
-  background-color: var(--content-bg);
-  box-shadow: var(--box-shadow);
-  border-radius: var(--border-radius);
   list-style: outside none none;
   transition: all 0.5s ease 0s;
-  :hover {
-    transform: translate3d(0,-2%,0);
-  }
 `
 
 export { Card }

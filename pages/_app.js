8d0884c6da224e2619090521b5c4a50779ec556e
DaTurboD <40794289+DaTurboD@users.noreply.github.com>
2021-07-20 20:49:37 +0200

    viewport

--- pages/_app.js
+++ pages/_app.js
@@ -33,7 +33,7 @@ class MyApp extends App {
     return (
       <>
         <Head>
-          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
 
           {/* DNS Prefetch*/} 
           <link rel="dns-prefetch" href="https://analytics.mxd.codes" />,

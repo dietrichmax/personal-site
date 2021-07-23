3ae2c8c0510838e3997d7d91b334d0633814c950
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-07-23 13:31:59 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -85,7 +85,6 @@ export default function About({ about }) {
               title={about.title}
               description=""
               slug={`about`}
-              aboutSchema
             />
 
             <ImageWrapper>

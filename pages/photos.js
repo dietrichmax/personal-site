949f6b06d084ede0fd821c5f3c588b5bcaf0cfd3
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-07-27 16:36:51 +0200

    Update photos.js

--- pages/photos.js
+++ pages/photos.js
@@ -6,28 +6,12 @@ import SEO from '@/components/seo/seo'
 import { useRouter } from 'next/router'
 import PageTitle from '@/components/title/page-title'
 import SubTitle from '@/components/title/sub-title'
+import Grid from '@/components/grid/grid'
 import PhotosPreview from "@/components/photo/photo-preview"
-import media from 'styled-media-query';
 
 const PhotosContainer = styled.div`
   margin: 0 auto;
   max-width: 1200px;
-`
-
-const Grid = styled.ol`
-  max-width: var(--width-container);
-  display: grid;
-  padding-left: var(--space);
-  padding-right: var(--space);
-  margin-bottom: var(--space-lg);
-  grid-template-columns: repeat(3,minmax(0,1fr));
-  gap: var(--space-lg);
-  list-style: none;
-  ${media.lessThan('medium')`
-    padding-left: var(--space-sm);
-    padding-right: var(--space-sm);
-    grid-template-columns: repeat(1, minmax(0px, 1fr));
-  `}
 `
 
 export default function Photos({ allPhotos }) {

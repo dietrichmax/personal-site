66d98b184be93f7944884e29b94b91d2b52ca86a
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-07-29 13:27:02 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -545,7 +545,7 @@ module.exports = withPlugins([
       "mxb.dev",
     ],
     deviceSizes: [272, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 272, 384, 1136, 1300],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 272, 346, 1136, 1300],
   }}
   ],{webpack (config, { dev, isServer }) {
       // Replace React with Preact only in client production build

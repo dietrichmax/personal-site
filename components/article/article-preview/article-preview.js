b074b04c20da35a242ebee75040b9493b7f423bd
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-07-29 12:44:40 +0200

    Update article-preview.js

--- components/article/article-preview/article-preview.js
+++ components/article/article-preview/article-preview.js
@@ -25,7 +25,6 @@ const CardItemInfo = styled.div`
 const CardItemTitle = styled.h2`
   font-size: 1.25rem;
   margin-bottom: 0.75rem;
-  margin-top: 0.75rem;
   color: var(--secondary-color);
   :hover {
     text-decoration: underline;

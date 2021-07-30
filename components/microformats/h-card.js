482d947f94e7108cc5e2445ccaf5087640ae6098
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-07-30 12:03:06 +0200

    Update h-card.js

--- components/microformats/h-card.js
+++ components/microformats/h-card.js
@@ -11,7 +11,7 @@ export default function hCard() {
         <Hidden> 
           <div className="h-card p-author" itemprop="author publisher" itemscope itemtype="http://schema.org/Person">
             <img className="u-photo" itemprop="image" src={config.siteLogo} alt={`Image of ${config.siteTitle}`}
-            <data className="p-description" itemprop="description" data={config.siteDescription} />  /> 
+            <data className="p-description" itemprop="description" data={config.siteDescription} /> 
             <data className="p-description" itemprop="url" data={config.siteUrl} />
             <a className="p-name u-url" rel="author" itemprop="name" href={config.siteUrl}>{config.siteTitleAlt}</a>
             <time className="dt-btday" dateTime="--08-08" itemprop="birthDate" />

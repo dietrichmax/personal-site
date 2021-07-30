853073a0bea42a14152b2f37d0e9775f452fac9a
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-07-30 12:02:36 +0200

    Update h-card.js

--- components/microformats/h-card.js
+++ components/microformats/h-card.js
@@ -9,14 +9,16 @@ export default function hCard() {
 
     return (
         <Hidden> 
-          <div className="h-card p-author">
-            <img className="u-photo" src={config.siteLogo} alt={`Image of ${config.siteTitle}`}  /> 
-            <a className="p-name u-url" rel="author" href={config.siteUrl}>{config.siteTitleAlt}</a>
-            <time className="dt-btday" dateTime="--08-08"></time>
-            <data className="p-locality" data="Rosenheim"></data>
-            <data className="p-region" data="Bavaria"></data>
-            <data className="p-postal-code" data="83026"></data>
-            <data className="p-country-name" data="Germany"></data>
+          <div className="h-card p-author" itemprop="author publisher" itemscope itemtype="http://schema.org/Person">
+            <img className="u-photo" itemprop="image" src={config.siteLogo} alt={`Image of ${config.siteTitle}`}
+            <data className="p-description" itemprop="description" data={config.siteDescription} />  /> 
+            <data className="p-description" itemprop="url" data={config.siteUrl} />
+            <a className="p-name u-url" rel="author" itemprop="name" href={config.siteUrl}>{config.siteTitleAlt}</a>
+            <time className="dt-btday" dateTime="--08-08" itemprop="birthDate" />
+            <data className="p-locality" data="Rosenheim" />
+            <data className="p-region" data="Bavaria" />
+            <data className="p-postal-code" data="83026" />
+            <data className="p-country-name" data="Germany" />
           </div>
         </Hidden> 
     )

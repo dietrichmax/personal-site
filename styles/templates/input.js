f14fe07bc87d7a454a72d2a57de809fd6062b0d7
Max Dietrich <40794289+DaTurboD@users.noreply.github.com>
2021-07-30 13:58:20 +0200

    Update input.js

--- styles/templates/input.js
+++ styles/templates/input.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 const Input = styled.input`
   height: 100%;
   padding-left: var(--space-sm);
-  margin-right: var(--space-sm);
   border: 2px solid var(--body-bg);
   background-color: var(--content-bg);
   color: var(--text-color);

b164ae5f87d6ecd6869df36d473234ec39e3ce4a
Max Dietrich <mail@mxd.codes>
2021-09-10 19:18:15 +0200

    github url

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -310,7 +310,7 @@ export default function Dashboard({
 
     const { forkCount } = githubStats.user.repository
     const stars = githubStats.user.repository.stargazers.totalCount
-    const githubUrl = "https://github.com/dietrichmax/"
+    const githubUrl = "https://mxd.codes/github"
     const forkUrl = `${githubStats.user.repository.url}/fork`
     const starUrl = githubStats.user.repository.url
     const lastModified = githubStats.user.repository.pushedAt

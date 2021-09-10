1a0dc94b09d9212f148e08cd832e8482fe09428e
Max Dietrich <mail@mxd.codes>
2021-09-10 18:22:04 +0200

    changed profileurl

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -310,7 +310,7 @@ export default function Dashboard({
 
     const { forkCount } = githubStats.user.repository
     const stars = githubStats.user.repository.stargazers.totalCount
-    const githubUrl = "https://github.com/DaTurboD/"
+    const githubUrl = "https://github.com/dietrichmax/"
     const forkUrl = `${githubStats.user.repository.url}/fork`
     const starUrl = githubStats.user.repository.url
     const lastModified = githubStats.user.repository.pushedAt

1e348a6493d5bdd7101c9461a963a98a7ffe8d64
Max Dietrich <40794289+dietrichmax@users.noreply.github.com>
2021-10-04 18:14:09 +0200

    Update cv.js

--- pages/cv.js
+++ pages/cv.js
@@ -269,14 +269,7 @@ export default function CV({ cv }) {
             </Paper>
             
             <Button 
-              onClick={() => domtoimage.toJpeg(document.getElementById('cv'), { height: 1053, width: 794 })
-              .then(function (dataUrl) {
-                  var link = document.createElement('a');
-                  link.download = 'cv.jpeg';
-                  link.href = dataUrl;
-                  link.click();
-              })
-          }
+              onClick={() => {window.open(`/api/cv`)}}
             >
               Download CV
             </Button> </ResumeWrapper>

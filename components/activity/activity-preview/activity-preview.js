70ae7a74b5945127df34004bed05977807b71755
Max Dietrich <40794289+dietrichmax@users.noreply.github.com>
2021-10-11 10:52:34 +0200

    Update activity-preview.js

--- components/activity/activity-preview/activity-preview.js
+++ components/activity/activity-preview/activity-preview.js
@@ -82,6 +82,9 @@ const Dot = styled.span`
   display: inline-block;
 `
 
+const staticMapLoader = ({ basemap, width, quality }) => {
+  return `https://static-maps-api.mxd.codes/img.php?basemap=${basemap || "stamen-terrain"}&attribution=${attribution || "none"}&width=${width || "520}&height={height || "250}&path[]=${path};weight:${weight || "3"};color:${color | "6680CA"}`}
+}
 
 export default function ActivityPreview({ activity }) {
 
@@ -148,7 +151,14 @@ export default function ActivityPreview({ activity }) {
         <HCard />
         <MapContainer>
           <Image
-            src={`https://static-maps-api.mxd.codes/img.php?basemap=stamen-terrain&attribution=none&width=520&height=250&path[]=${JSON.stringify(path)};weight:3;color:6680CA`}
+            loader={staticMapLoader}
+            basemap="stamen-terrain"
+            attribution="none"
+            width="520"
+            height="250"
+            path={JSON.stringify(path)}
+            weight="3"
+            color="6680CA"
             layout="fill"
            />
         </MapContainer>

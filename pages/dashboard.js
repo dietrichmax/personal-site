870abc537dee158b3cf164e65a0e5317f2bf09de
Max Dietrich <40794289+dietrichmax@users.noreply.github.com>
2021-10-13 12:07:52 +0200

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -363,7 +363,7 @@ export default function Dashboard({
     })
     
     console.log(parseInt(liveViews.visitors) > 1 ? "true" : "false")
-    console.log(parseInt(liveViews.visitors))
+    console.log(liveViews.visitors)
     return (
         <>
             <Layout>

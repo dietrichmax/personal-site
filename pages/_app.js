2b92eac632803837a47c66a66169a9ff5c479556
Max Dietrich <mail@mxd.codes>
2022-04-29 11:21:51 +0200

    test

--- pages/_app.js
+++ pages/_app.js
@@ -93,6 +93,8 @@ class MyApp extends App {
           {/*Manifest.json  */}
           <link href="/manifest.json" rel="manifest" />
 
+          {/*Adsense  */}
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0037698828864449" crossorigin="anonymous"></script>
         </Head>
         <Providers>
           <GlobalStyle/>

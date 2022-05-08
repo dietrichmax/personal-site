e1b2aef65fe6ce15dbd4f8e4e2d5f2b4c47cbdf2
Max Dietrich <mail@mxd.codes>
2022-05-08 22:02:03 +0200

    title

--- src/data/internal/SiteConfig.js
+++ src/data/internal/SiteConfig.js
@@ -1,10 +1,10 @@
 const config = {
-  siteTitle: "Max Dietrich - GeoData-Manager", // Site title.
+  siteTitle: "Max Dietrich - Geospatial Developer", // Site title.
   siteTitleShort: "mxd.codes", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "Max Dietrich", // Alternative site title for SEO.
-  siteSubtitle: "Geodata-Manager",
+  siteSubtitle: "Geospatial Developer",
   siteDescription:
-    "Hi, I'm Max. I currently work as a Geodata Manager at RIWA at where I'm doing Data Migrations. Beside that I ride my mountain bike in the alps, code and design my website and publish new content whenever i can.", // Website description used for RSS feeds/meta description tag.
+    "Currently i work as IT-Architect at Bayernwerk Netz GmbH. Beside that I ride my mountain bike in the alps, code and design my website and publish new content on my website whenever i can.", // Website description used for RSS feeds/meta description tag.
   siteLogo: "/logos/firefox/firefox-marketplace-512-512.jpg", // Logo used for SEO and manifest.
   siteUrl: "https://mxd.codes", // Domain of your website.
   domain: "mxd.codes",

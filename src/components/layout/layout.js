2f0e45663326ee906d01894caa098012f60d505b
Max Dietrich <mail@mxd.codes>
2022-10-23 08:47:53 +0200

    ol style

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -6,7 +6,7 @@ import Footer from "@/components/navigation/footer/footer"
 require("prismjs/components/prism-python")
 require("prismjs/plugins/line-numbers/prism-line-numbers.js")
 require("prismjs/plugins/show-language/prism-show-language.js")
-require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+//require("prismjs/plugins/line-numbers/prism-line-numbers.css")
 
 const Container = styled.div``
 

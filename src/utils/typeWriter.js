058855853f9c8d7bad4160d114e3511d0f37ba14
Max Dietrich <40794289+dietrichmax@users.noreply.github.com>
2023-02-27 15:16:54 +0100

    Update typeWriter.js

--- src/utils/typeWriter.js
+++ src/utils/typeWriter.js
@@ -5,6 +5,7 @@ import media from "styled-media-query"
 const HeroSubTitle = styled.div`
   font-weight: normal;
   font-size: 60px;
+  color: rgb(255, 255, 255);
   ${media.lessThan("large")`
     padding: 0 var(--space-sm);
   `}

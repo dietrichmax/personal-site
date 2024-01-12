19b00287e5eae61e94dff816f751a3c27e36313f
Max Dietrich <mail@mxd.codes>
2024-01-12 21:16:40 +0100

    fixed dependencies

--- src/utils/providers.js
+++ src/utils/providers.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react"
 import { lightTheme, darkTheme } from "@/styles/themes"
-import useDarkMode from "use-dark-mode"
 import { ThemeProvider } from "styled-components"
 
 export default function Providers({ children }) {
-  const { value } = useDarkMode(false, { storageKey: null, onChange: null })
+  const isDarkMode = () =>
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  console.log(isDarkMode)
   const theme = value ? darkTheme : lightTheme
 
   const [mounted, setMounted] = useState(false)

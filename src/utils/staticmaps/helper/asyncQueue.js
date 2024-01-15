5a48cdc6a3b91de4f6fd6426b6279dca6330dcc3
Max Dietrich <mail@mxd.codes>
2024-01-15 09:38:48 +0100

    removed staticmaps

---
+++ src/utils/staticmaps/helper/asyncQueue.js
@@ -0,0 +1,8 @@
+const workOnQueue = async (queue, index = 0) => {
+  if (!queue[index]) return true // Finished
+  await queue[index]()
+  await workOnQueue(queue, index + 1)
+  return false
+}
+
+export default workOnQueue

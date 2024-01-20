cce490089507d263b77f8193ad7c458e80b092f0
Max Dietrich <mail@mxd.codes>
2024-01-20 16:46:05 +0100

    test

--- next.config.js
+++ next.config.js
@@ -14,16 +14,16 @@ const ContentSecurityPolicy = `
 `
 
 const securityHeaders = [
-  /*/ https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
     key: "Content-Security-Policy",
     value: ContentSecurityPolicy.replace(/\n/g, ""),
-  },*/
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  },
+  /*s/ https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
     key: "Referrer-Policy",
     value: "origin-when-cross-origin",
-  },
+  },*/
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
     key: "X-Frame-Options",

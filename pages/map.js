19e1b681f8a6507523793bbc698c3467fe2629b3
Max Dietrich <mail@mxd.codes>
2024-01-23 13:31:46 +0100

    revalidate

--- pages/map.js
+++ pages/map.js
@@ -82,7 +82,7 @@ export async function getStaticProps() {
   await client.end()
 
   return {
-    revalidate: 86400,
+    revalidate: 600,
     props: {
       location: recentLocation.rows[0],
       locationsCount: locationsCount.rows[0].count,
